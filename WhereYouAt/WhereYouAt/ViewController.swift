9e213d141152df83be0e59d31153d43f0b677759
Cory Jbara <cjbara@nd.edu>
2016-04-03 20:22:02 -0400

    Adding Xcode Project

---
+++ WhereYouAt/WhereYouAt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhereYouAt
+//
+//  Created by Cory Jbara on 4/3/16.
+//  Copyright © 2016 Where You At. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
