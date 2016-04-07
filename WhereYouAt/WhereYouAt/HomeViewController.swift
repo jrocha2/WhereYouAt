fab08c5b88af92302f00bc1de806c682759161a9
Cory Jbara <cjbara@nd.edu>
2016-04-06 23:01:36 -0400

    Sends database instance to new viewcontrollers

--- WhereYouAt/WhereYouAt/HomeViewController.swift
+++ WhereYouAt/WhereYouAt/HomeViewController.swift
@@ -31,14 +31,20 @@ class HomeViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "createStatus" {
+            let navController = segue.destinationViewController as! UINavigationController
+            let dest = navController.topViewController as! LocationsTableViewController
+            dest.db = self.db
+        }
+        
     }
-    */
+ 
 
 }
